a1a785ad1d978e48cf9623b0891c21736d1cf664
Andrew Williams <williams.andrew@gmail.com>
2015-03-10 21:43:41 -0500

    Initial implementation of post rename
    Initial shot at supporting a "post exchange" rename of oldfile. This
    allows for the following pattern:
    
      Swap    `/tmp/foo` with `/tmp/bar`
      Rename  `/tmp/bar` to `/tmp/oldbar`

--- xchg.c
+++ xchg.c
@@ -3,12 +3,15 @@
 #define VERSION "0.1.0"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <errno.h>
 #include <sysexits.h>
 #include <string.h>
 #include <fcntl.h>
+#include <libgen.h>
+#include <linux/limits.h>
 
 //TODO Replace this with <linux/fs.h>
 #define RENAME_NOREPLACE    1   /* Don't overwrite target */
@@ -27,14 +30,24 @@ int do_exchange(char *src, char *dst)
     return renameat2(AT_FDCWD, src, AT_FDCWD, dst, RENAME_EXCHANGE);
 }
 
+int do_rename(char *src, char *dst)
+{
+    return renameat2(AT_FDCWD, src, AT_FDCWD, dst, RENAME_NOREPLACE);
+}
+
 int main(int argc, char *argv[])
 {
     int c, verbose, numfiles;
+    char *pname = NULL;
+    char newname[PATH_MAX];
 
-    while ((c = getopt(argc, argv, "vV")) != -1) {
+    while ((c = getopt(argc, argv, "vVn:")) != -1) {
         switch (c) {
         case 'v':
             verbose = 1;
+            break;
+        case 'n':
+            pname = optarg;
             break;
         case 'V':
             printf("Version: %s\n", VERSION);
@@ -53,6 +66,20 @@ int main(int argc, char *argv[])
         return EX_USAGE;
     }
 
+    if (pname) {
+        if (*pname == '/') {
+            strncpy(newname, pname, PATH_MAX);
+        } else {
+            // Determine the parent directory
+            char *bname = (char *) malloc(sizeof newname);
+            strncpy(bname, argv[optind], PATH_MAX);
+            dirname(bname);
+
+            //TODO this code needs length/error checking
+            snprintf(newname, PATH_MAX, "%s/%s", bname, pname);
+        }
+    }
+
     if (verbose)
         printf("Exchanging %s <-> %s\n", argv[optind], argv[optind+1]);
 
@@ -68,6 +95,24 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Cannot exchange files. Error %d - %s\n",
                             errno, strerror(errno));
             return EX_OSERR;
+        }
+    }
+
+    if (pname) {
+        if (do_rename(argv[optind], newname) != 0) {
+            printf("Rename error\n");
+            if (errno == ENOSYS) {
+                // If we get here its likely because the kernel does not support
+                // renameat2
+                fprintf(stderr, "Cannot exchange files\n\nConfirm your kernel "
+                        "supports the `renameat2` syscall and that "
+                        "your filesystem supports atomic exchange\n");
+                return EX_OSERR;
+            } else {
+                fprintf(stderr, "Cannot exchange files. Error %d - %s\n",
+                                errno, strerror(errno));
+                return EX_OSERR;
+            }
         }
     }
 
